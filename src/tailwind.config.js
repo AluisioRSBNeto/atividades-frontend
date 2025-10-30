206ce18894017c2308868cee3a11c90d7360586f
AluisioRSBNeto <aluisio.ramalho@yahoo.com.br>
2025-10-30 18:19:44 -0300

    Delete src directory

--- src/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: 'media', // muda conforme o tema do sistema
-  plugins: [],
-};
